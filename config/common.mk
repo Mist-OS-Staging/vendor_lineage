c5f0bb90c2b6fbb254bf481b52f85a213103d88e
LuK1337 <priv.luk@gmail.com>
2025-02-11 20:59:18 +0100

    overlay: Set PermissionController help URLs
    Change-Id: Ib21950062261e8f280c16544341c35f2055c554a

--- config/common.mk
+++ config/common.mk
@@ -245,7 +245,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
-    NetworkStackOverlay
+    NetworkStackOverlay \
+    PermissionControllerOverlay
 
 # Translations
 CUSTOM_LOCALES += \
